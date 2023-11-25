9dcfc6cb9a235615b42ef159ba7d8f1405aa83c7
Kamlesh <banosekar.kamlesh@gmail.com>
2023-11-25 17:23:11 +0530

    basic changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core_module/header/header.component';
+import { FooterComponent } from './core_module/footer/footer.component';
+import { HomeComponent } from './core_module/home/home.component';
+import { NavigationComponent } from './core_module/navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
