a71b50d6bed59e7cf62c4bb71fc93b0de428a08b
Kamlesh <banosekar.kamlesh@gmail.com>
2023-12-29 17:08:03 +0530

    requirement form array is ready

--- src/app/feature_module/requirement/requirement.component.ts
+++ src/app/feature_module/requirement/requirement.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-requirement',
@@ -10,25 +16,56 @@ export class RequirementComponent implements OnInit {
   requirementForm!: FormGroup;
   selectedFile: any = null;
 
+  constructor(private fb: FormBuilder) {}
+
   ngOnInit(): void {
     this.pageForm();
   }
 
+  // requirement form
   pageForm() {
-    this.requirementForm = new FormGroup({
-      fullName: new FormControl(''),
-      email: new FormControl(''),
-      phoneNumber: new FormControl(''),
-      subject: new FormControl(''),
-      companyName: new FormControl(''),
-      width: new FormControl(''),
-      height: new FormControl(''),
-      comments: new FormControl(''),
-      selectType: new FormControl(''),
+    this.requirementForm = this.fb.group({
+      fullName: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      phoneNumber: new FormControl('', [
+        Validators.required,
+        Validators.min(10),
+      ]),
+      projects: this.fb.array([this.newRequirement()]),
+      projectRequirement: this.fb.group({
+        subject: new FormControl(''),
+        companyName: new FormControl('', [Validators.required]),
+        width: new FormControl('', [Validators.required]),
+        height: new FormControl('', [Validators.required]),
+        comments: new FormControl(''),
+        selectType: new FormControl('', [Validators.required]),
+      }),
     });
   }
 
-  onFileSelected(event: any): void {
-    this.selectedFile = event.target.files[0] ?? null;
+  // getting projects form array control
+  get projects(): FormArray {
+    return this.requirementForm.controls['projects'] as FormArray;
+  }
+
+  // form array controls
+  newRequirement(): FormGroup {
+    return this.fb.group({
+      subject: '',
+      companyName: '',
+      width: '',
+      height: '',
+      comments: '',
+      selectType: '',
+    });
+  }
+
+  // adding form array controls to projects
+  onAddRequirement() {
+    this.projects.push(this.newRequirement());
+  }
+
+  onRequirementSubmitted() {
+    console.log(this.requirementForm);
   }
 }
