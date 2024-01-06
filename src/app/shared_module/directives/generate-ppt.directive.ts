ec23c2c4d2ac4b6941893173dbae7cb3f4722963
Kamlesh <banosekar.kamlesh@gmail.com>
2024-01-06 16:25:12 +0530

    Static presentation completed

--- src/app/shared_module/directives/generate-ppt.directive.ts
+++ src/app/shared_module/directives/generate-ppt.directive.ts
@@ -34,9 +34,11 @@ export class GeneratePptDirective implements OnInit {
     this.genSlide01(this.pptx);
     this.genSlide02(this.pptx);
     this.genSlide03(this.pptx);
+    this.genSlide04(this.pptx);
+    this.genSlideEnd(this.pptx);
   }
 
-  genSlide01(pptx: any) {
+  genMasterSlide(pptx: any) {
     pptx.layout = 'LAYOUT_WIDE';
 
     pptx.defineSlideMaster({
@@ -46,7 +48,7 @@ export class GeneratePptDirective implements OnInit {
       objects: [
         {
           // slide bottom rect angle
-          rect: { x: 0, y: 6.9, w: '100%', h: 0.7, fill: { color: '003b75' } },
+          rect: { x: 0, y: 7.1, w: '100%', h: 0.5, fill: { color: '003b75' } },
         },
         {
           // slide bottom rect angle text
@@ -54,7 +56,7 @@ export class GeneratePptDirective implements OnInit {
             text: 'S.T.A.R. Laboratories - Confidential',
             options: {
               x: 0,
-              y: 7.3,
+              y: 7.4,
               w: '100%',
               align: 'center',
               color: 'FFFFFF',
@@ -66,20 +68,22 @@ export class GeneratePptDirective implements OnInit {
           // slide bottom rect angle image
           image: {
             x: 11.45,
-            y: 7,
+            y: 7.2,
             w: 1.67,
-            h: 0.5,
+            h: 0.3,
             path: 'https://cdn.pixabay.com/photo/2014/05/26/13/32/butterfly-354528_1280.jpg',
           },
         },
       ],
-      slideNumber: { x: 1.0, y: 7.1, color: 'FFFFFF' }, // Slide Number
+      slideNumber: { x: 0.5, y: 7.2, color: 'FFFFFF' }, // Slide Number
     });
-
+  }
+  genSlide01(pptx: any) {
+    this.genMasterSlide(this.pptx);
     let slide = pptx.addSlide({ masterName: 'MASTER_SLIDE' });
     slide.addText('How To Create PowerPoint Presentations with JavaScript', {
       x: 0,
-      y: '50%',
+      y: '45%',
       w: '100%',
       fontSize: 34,
       color: '003b75',
@@ -88,46 +92,133 @@ export class GeneratePptDirective implements OnInit {
       valign: 'middle',
       shadow: { type: 'outer', opacity: 0.3 },
     });
-  }
-  genSlide02(pptx: any) {
-    let slideHeading = 'This is first hello world slide';
-    let options = {
-      x: 0.0,
-      y: 0.0,
+    slide.addText('City', {
+      x: 0,
+      y: '60%',
       w: '100%',
-      h: 1.5, // add height to text
+      fontSize: 28,
+      color: '003b75',
+      bold: true,
       align: 'center',
-      fontSize: 24,
-      color: 'f00000',
-      fill: '000000',
-    };
-    let slide = pptx.addSlide();
-    slide.addText(slideHeading, options);
+      valign: 'middle',
+    });
+  }
+
+  genSlide02(pptx: any) {
+    let slideHeading = 'Sr 15 Janta Hardware  96x36”  GSB';
+    let type = 'Vinyl+Sunboard';
+    let slideImage =
+      'https://cdn.pixabay.com/photo/2014/05/26/13/32/butterfly-354528_1280.jpg';
+
+    let slide = pptx.addSlide({ masterName: 'MASTER_SLIDE' });
+    slide.addText(slideHeading, {
+      x: 0.0,
+      y: 0.1,
+      w: '100%',
+      h: 0.8, // add height to text
+      align: 'center',
+      fontSize: 30,
+      bold: true,
+      color: '003b75',
+    });
+    slide.addText(type, {
+      x: 0.0,
+      y: 0.8,
+      w: '100%',
+      h: 0.8, // add height to text
+      align: 'center',
+      fontSize: 27,
+      color: '003b75',
+    });
+    slide.addImage({
+      path: slideImage,
+      x: '15%',
+      y: '22%',
+      sizing: {
+        type: 'contain',
+        w: '70%',
+        h: '70%',
+      },
+    });
     //Save the Presentation file name
   }
   genSlide03(pptx: any) {
     let slideHeading = 'This is Second slide';
     let slideImage =
       'https://cdn.pixabay.com/photo/2014/05/26/13/32/butterfly-354528_1280.jpg';
-    let options = {
+
+    let slide = pptx.addSlide({ masterName: 'MASTER_SLIDE' });
+    slide.addImage({
+      path: slideImage,
       x: 0.0,
-      y: 0.0,
+      y: 0.2,
+      sizing: {
+        type: 'contain',
+        w: '90%',
+        h: '90%',
+      },
+    });
+  }
+
+  genSlide04(pptx: any) {
+    let slideHeading = 'Sr 15 Janta Hardware  32x32”  GSB';
+    let type = 'Vinyl+Sunboard';
+    let slideImage =
+      'https://cdn.pixabay.com/photo/2014/05/26/13/32/butterfly-354528_1280.jpg';
+
+    let slide = pptx.addSlide({ masterName: 'MASTER_SLIDE' });
+    slide.addText(slideHeading, {
+      x: 0.0,
+      y: 0.1,
       w: '100%',
-      h: 1.5, // add height to text
+      h: 0.8, // add height to text
       align: 'center',
-      fontSize: 24,
-      color: 'f00000',
-      fill: '000000',
-    };
-    let slide = pptx.addSlide();
-    slide.addText(slideHeading, options);
+      fontSize: 30,
+      bold: true,
+      color: '003b75',
+    });
+    slide.addText(type, {
+      x: 0.0,
+      y: 0.8,
+      w: '100%',
+      h: 0.8, // add height to text
+      align: 'center',
+      fontSize: 27,
+      color: '003b75',
+    });
     slide.addImage({
       path: slideImage,
       x: '15%',
-      y: '30%',
-      w: '65%',
-      h: '65%',
+      y: '22%',
+      sizing: {
+        type: 'contain',
+        w: '70%',
+        h: '70%',
+      },
     });
+    //Save the Presentation file name
+  }
+
+  // End slide
+  genSlideEnd(pptx: any) {
+    pptx.defineSlideMaster({
+      title: 'END_SLIDE',
+      margin: [0.5, 0.25, 1.0, 0.25],
+      background: { color: '003b75' }, //add background color to slide
+    });
+    let slide = pptx.addSlide({ masterName: 'END_SLIDE' });
+    slide.addText('Thank You !', {
+      x: 0,
+      y: '50%',
+      w: '100%',
+      fontSize: 50,
+      color: 'FFFFFF',
+      bold: true,
+      align: 'center',
+      valign: 'middle',
+      shadow: { type: 'outer', opacity: 0.3 },
+    });
+
     this.pptx.writeFile({ fileName: 'Sample Presentation.pptx' });
   }
 }
